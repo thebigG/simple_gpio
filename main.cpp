f9ef98ff5df85fb719488d0d3e54b07a1208824d
thebigg <yunior.eury@gmail.com>
2022-05-04 22:54:03 -0500

    -Configure CMake with Boost. -TODO:Run test with all GPIO pins.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,59 @@ Jeff Tranter <jtranter@ics.com>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string>
+#include <iostream>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+namespace po = boost::program_options;
+
+
+void show_help(const po::options_description& desc, const std::string& topic = "")
+{
+	std::cout << desc << '\n';
+	if (topic != "") {
+		std::cout << "You asked for help on: " << topic << '\n';
+	}
+	exit( EXIT_SUCCESS );
+}
+
+void process_program_options(const int argc, const char *const argv[])
+{
+	po::options_description desc("Usage");
+	desc.add_options()
+		(
+			"help,h",
+			po::value< std::string >()
+				->implicit_value("")
+				->notifier(
+					[&desc](const std::string& topic) {
+						show_help(desc, topic);
+					}
+				),
+			"Show help. If given, show help on the specified topic."
+		)
+	;
+
+	if (argc == 1) {
+		show_help(desc); // does not return
+	}
+
+	po::variables_map args;
+
+	try {
+		po::store(
+			po::parse_command_line(argc, argv, desc),
+			args
+		);
+	}
+	catch (po::error const& e) {
+		std::cerr << e.what() << '\n';
+		exit( EXIT_FAILURE );
+	}
+	po::notify(args);
+	return;
+}
 
 int main()
 {
